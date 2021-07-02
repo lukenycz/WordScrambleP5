97439db90738fbdc6fa5a44dc46294fbd9d22d6b
Łukasz Nycz <lnycz96@gmail.com>
2021-07-02 18:17:55 +0200

    Checking for valid answers

--- WordScrambleP5/ViewController.swift
+++ WordScrambleP5/ViewController.swift
@@ -92,6 +92,7 @@ class ViewController: UITableViewController {
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         return misspelledRange.location == NSNotFound
+        
     }
 }
 
